bc9167bce99aa1536443a6f5281aa5fc6b8032fb
Hector <Hector>
2021-11-20 14:35:48 -0500

    se creo  el usuario y las credenciales modelos.

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,3 +1,5 @@
 export * from './empleado-model.repository';
 export * from './cliente.repository';
 export * from './contrato.repository';
+export * from './user-credentials.repository';
+export * from './user.repository';
